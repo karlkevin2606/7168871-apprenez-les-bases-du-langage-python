f82163dcec6228619ca66058dd6077c524aeabef
karlkevin2606 <tefackkarlkevin@yahoo.com>
2024-06-23 22:44:42 +0100

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,8 @@
 # Écrivez votre code ici !
+nom = karl_kevin
+age = 25
+print(f"je m'appelle {nom} et j'ai {age} ans.")
+age = 10
+print(f"je m'appelle {nom} et j'ai {age} ans.")
+print("je sais , je suis trop fort")
+print("mais bon c'est juste les debuts")
